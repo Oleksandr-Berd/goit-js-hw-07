674b144b2f5a4179cbaac320fb0053795f2d3350
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-11 20:44:24 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imagesRef = galleryItems.map((element) => {
+  return `<div class="gallery__image">
+  <a class="gallery__link" href="${element.original}">
+  <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="l${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
+});
+
+const myGallery = document.querySelector(".gallery");
+
+myGallery.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+
+console.log(myGallery);
