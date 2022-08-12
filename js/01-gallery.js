c51bb2a2ef2bfa5f008e77e93d3e3e22ccce3c73
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-12 22:12:34 +0300

    study

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,50 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const imagesRef = galleryItems.map((element) => {
-  return `<div class="gallery__image">
+// const basicLightbox = require("basiclightbox");
+
+// import * as basicLightbox from "basicLightbox";
+
+const galleryLayout = createLayout(galleryItems);
+
+function createLayout(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `<div class="gallery__image">
   <a class="gallery__link" href="${element.original}">
   <img
       class="gallery__image"
       src="${element.preview}"
-      data-source="l${element.original}"
+      data-source="${element.original}"
       alt="${element.description}"
     />
   </a>
 </div>`;
-});
+    })
+    .join("");
+}
 
 const myGallery = document.querySelector(".gallery");
 
-myGallery.insertAdjacentHTML("afterbegin", imagesRef.join(""));
+myGallery.insertAdjacentHTML("afterbegin", galleryLayout);
 
-console.log(myGallery);
+myGallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const urlImg = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${urlImg}" width="800" height="600">
+`);
+
+  instance.show();
+
+  window.addEventListener("keydown", onEscPress);
+
+  function onEscPress(evt) {
+    if (evt.code === "Tab") {
+      instance.close();
+    }
+  }
+}
