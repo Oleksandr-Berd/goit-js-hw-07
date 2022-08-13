49e8558234a419ce0e5a4d957afda95e7898c615
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-13 19:44:23 +0300

    іі

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const lightboxLayout = createLayout(galleryItems);
+
+function createLayout(galleryItems) {
+  return galleryItems
+    .map((element) => {
+      return `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const myGallery = document.querySelector(".gallery");
+
+myGallery.insertAdjacentHTML("afterbegin", lightboxLayout);
+
+myGallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const urlImg = evt.target;
+
+  let lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: `alt`,
+    captionDelay: 250,
+  });
+
+  console.log(lightbox);
+}
+
+console.log(myGallery);
